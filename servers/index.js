18fe58ca4c912b27a33685c7e428f327b1eb2cc0
Kumar Harsh <kharsh39@gmail.com>
2019-09-04 12:00:29 +0530

    onboarding server complete

--- servers/index.js
+++ servers/index.js
@@ -1,12 +1,20 @@
 var http = require('http');
 var MongoClient = require('mongodb').MongoClient;
-var DBurl = "mongodb://localhost:8081/mydb";
-
 
 var sign_up_server_port = 8081;
+var login_server_port = 8082;
+
+var RESPONSE_DUPLICATE = "DUPLICATE";
+var RESPONSE_SUCCESSFUL = "SUCCESSFUL";
+var RESPONSE_FAILED = "FAILED";
+var RESPONSE_TIMED_OUT = "TIMED OUT";
+var RESPONSE_INVALID_CRED = "INVALID CREDENTIALS";
+
+
+var usersDB = "SaveHerFromCancerUsers"
+var usersDBurl = "mongodb://localhost:27017/" + usersDB;
 
 http.createServer(function (request, response) {
-
     request.on('data', function (message) {
         var messageString = message.toString().split(" ");
         var email = messageString[0];
@@ -15,13 +23,15 @@ http.createServer(function (request, response) {
         var userType = messageString[3];
         var userLocation = messageString[4];
 
-        MongoClient.connect(DBurl, function (err, db) {
+        MongoClient.connect(usersDBurl, function (err, client) {
+            var db = client.db("mongodb");
             if (err) {
-                // response.write(err);
-                // response.end();
                 throw err
             };
+
+            var userID = "user_" + Math.floor(Math.random() * 1000000000);
             var user = {
+                id: userID,
                 name: userName,
                 email: email,
                 password: password,
@@ -29,17 +39,50 @@ http.createServer(function (request, response) {
                 location: userLocation
             }
 
-            console.log(user);
-            dbo.collection("users").insertOne(user, function (err, res) {
-                if (err) {
-                    // response.write(err);
-                    // response.end();
-                    throw err;
+            db.collection("users").find(user).toArray(function (err, result) {
+                if (err) throw err;
+                console.log(result);
+                if (result.length <= 0){
+                    db.collection("users").insertOne(user, function(error, result){
+                        if(error){
+                            response.write(error.toString());
+                            response.end();
+                        } else {
+                            response.write(RESPONSE_SUCCESSFUL);
+                            response.end();
+                        }
+                    });
+                }else{
+                    response.write(RESPONSE_DUPLICATE);
+                    response.end();
                 }
-                response.write("1 document inserted");
-                db.close();
-                response.end();
             });
         });
     });
 }).listen(sign_up_server_port);
+
+http.createServer(function(request, response){
+
+    request.on('data', function(message){
+        var messageString = message.toString().split(" ");
+        var email = messageString[1];
+        var password = messageString[0];
+
+        db.collection("users").find({email: email}).toArray(function(err, result){
+            if(err || result.length<=0){
+                response.write(RESPONSE_FAILED);
+                response.end();
+            } else {
+                if(result[0].data().password === password){
+                    response.write(RESPONSE_SUCCESSFUL);
+                    response.write("\ntoken: " + result[0].data().id);
+                    response.end();
+                } else {
+                    response.write(RESPONSE_FAILED);
+                    response.write(RESPONSE_INVALID_CRED);
+                    response.end();
+                }
+            }
+        });
+    });
+}).listen(login_server_port);
