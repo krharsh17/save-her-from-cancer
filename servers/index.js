b114e0904b80a572349fe6a716f991721aca43f1
Kumar Harsh <kharsh39@gmail.com>
2019-09-05 13:04:04 +0530

    login server under construction

--- servers/index.js
+++ servers/index.js
@@ -9,6 +9,8 @@ var RESPONSE_SUCCESSFUL = "SUCCESSFUL";
 var RESPONSE_FAILED = "FAILED";
 var RESPONSE_TIMED_OUT = "TIMED OUT";
 var RESPONSE_INVALID_CRED = "INVALID CREDENTIALS";
+var RESPONSE_LOGGED_IN = "LOG IN ";
+var RESPONSE_LOGGED_OUT = "LOG OUT ";
 
 
 var usersDB = "SaveHerFromCancerUsers"
@@ -36,15 +38,16 @@ http.createServer(function (request, response) {
                 email: email,
                 password: password,
                 user_type: userType,
-                location: userLocation
+                location: userLocation,
+                login_state: true
             }
 
-            db.collection("users").find(user).toArray(function (err, result) {
+            db.collection("users").find({ email: email }).toArray(function (err, result) {
                 if (err) throw err;
                 console.log(result);
-                if (result.length <= 0){
-                    db.collection("users").insertOne(user, function(error, result){
-                        if(error){
+                if (result.length <= 0) {
+                    db.collection("users").insertOne(user, function (error, result) {
+                        if (error) {
                             response.write(error.toString());
                             response.end();
                         } else {
@@ -52,7 +55,7 @@ http.createServer(function (request, response) {
                             response.end();
                         }
                     });
-                }else{
+                } else {
                     response.write(RESPONSE_DUPLICATE);
                     response.end();
                 }
@@ -61,28 +64,78 @@ http.createServer(function (request, response) {
     });
 }).listen(sign_up_server_port);
 
-http.createServer(function(request, response){
+http.createServer(function (request, response) {
 
-    request.on('data', function(message){
+    request.on('data', function (message) {
         var messageString = message.toString().split(" ");
         var email = messageString[1];
         var password = messageString[0];
+        var state_lock = messageString[2];
 
-        db.collection("users").find({email: email}).toArray(function(err, result){
-            if(err || result.length<=0){
-                response.write(RESPONSE_FAILED);
-                response.end();
-            } else {
-                if(result[0].data().password === password){
-                    response.write(RESPONSE_SUCCESSFUL);
-                    response.write("\ntoken: " + result[0].data().id);
+        MongoClient.connect(usersDBurl, function (err, client) {
+            var db = client.db("mongodb");
+            if (err) {
+                throw err
+            };
+
+            db.collection("users").find({ email: email }).toArray(function (err, result) {
+                if (err || result.length <= 0) {
+                    response.write(RESPONSE_FAILED);
                     response.end();
                 } else {
-                    response.write(RESPONSE_FAILED);
-                    response.write(RESPONSE_INVALID_CRED);
-                    response.end();
+                    if (result[0].password === password) {
+                        if (result[0].login_state === true) {
+                            db.collection("users").updateOne({ email: result[0].email }, {
+                                $set: { login_state: false }
+                            }, function (err, res) {
+                                if (err) {
+                                    response.write(RESPONSE_FAILED);
+                                    response.end();
+                                    console.log(err.toString());
+                                }
+                                response.write("\ntoken: " + result[0].id);
+                                response.write(RESPONSE_LOGGED_OUT + RESPONSE_SUCCESSFUL);
+                                response.end();
+                            });
+                        } else {
+                            db.collection("users").updateOne({ email: result[0].email }, {
+                                $set: { login_state: true },
+                            }, function (err, res) {
+                                if (err) {
+                                    response.write(RESPONSE_FAILED);
+                                    response.end();
+                                    console.log(err.toString());
+                                }
+                                if (state_lock !== true)
+                                    setTimeout(login_timeout(result[0].id, db), 4 * 60 * 60 * 1000);
+
+                                response.write(RESPONSE_LOGGED_IN + RESPONSE_SUCCESSFUL);
+                                response.write("\ntoken: " + result[0].id);
+                                response.end();
+
+                            });
+                        }
+                    } else {
+                        response.write(RESPONSE_FAILED);
+                        response.write(RESPONSE_INVALID_CRED);
+                        response.end();
+                    }
                 }
-            }
+            });
         });
     });
 }).listen(login_server_port);
+
+function login_timeout(id, db) {
+    db.collection("users").updateOne(
+        { id: id },
+        { $set: { login_state: false } },
+        function (err, res) {
+            if (err)
+                console.log(err.toString());
+
+            console.log(res.toString());
+        }
+    );
+
+}
